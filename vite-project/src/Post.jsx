87ff71823d82be4b46652fb0ec4014207df0137a
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-05-09 22:37:45 -0300

    feature/learning-props

---
+++ vite-project/src/Post.jsx
@@ -0,0 +1,12 @@
+export const Post = ( props ) => {
+
+    console.log(props)
+
+    return (
+        <div>
+            <strong>{props.author}</strong>
+            <p>{props.content}</p>
+        </div>
+    )
+
+}
