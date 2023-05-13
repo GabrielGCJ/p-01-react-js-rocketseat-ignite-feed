a44c8087c2d1478e83e0a8357687efe21e034a94
gabrielj <gabriel.ggcj@gmail.com>
2023-05-12 22:25:49 -0300

    feature/learning-comment-form

--- vite-project/src/components/Sidebar/Sidebar.jsx
+++ vite-project/src/components/Sidebar/Sidebar.jsx
@@ -4,7 +4,6 @@ export const Sidebar = () => {
 
     return (
         <aside className={styles.sidebar}>
-            Sidebar Top
             <img className={styles.cover} src="https://images.unsplash.com/photo-1604964432806-254d07c11f32?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50" alt="papel-de-parede-do-usuario" />
             <div className={styles.profile}>
                 <img className={styles.avatar} src="https://avatars.githubusercontent.com/u/91347602?v=4"/>
